98db0b5194680ff497fb7de21acefe0d41e0f2ce
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-11-08 09:19:21 +0100

    Add the project directories

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Homepage.scss";
+import Directory from './../../components/directory/Directory';
+
+export const Homepage = () => {
+  return (
+    <div className='Homepage'>
+      <Directory />
+    </div>
+  );
+};
