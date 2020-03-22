217d14f7bc40c8303f44a28cdef7ae996d52063d
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-03-22 14:29:11 +0100

    Add the signin component to the app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import { Homepage } from "./pages/homepage/Homepage";
 import ShopPage from "./pages/homepage/shop/ShopPage";
 import Header from "./components/header/Header";
+import SignInAndSignUp from './pages/homepage/signIn_signOut/SignIn_Out';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
